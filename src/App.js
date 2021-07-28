3c83b2f328b10835de6ab38a536e2cd0209a1d90
kk-smiles <thenotsorealkk@gmail.com>
2021-07-28 18:59:18 +0630

    one more change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h1> Testing netlify free hosting for create-react-app </h1>
       <h2> Another change to test for continuous deployment </h2>
+      <h2> One more change to test for git visiblity settings </h2>
     </div>   
   );
 }
